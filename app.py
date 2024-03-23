9fad8d1319452a0aeffba5ee6ec0b233c75c81bc
jdicao <jeffdicao@gmail.com>
2024-03-22 21:37:29 -0500

    nuevos endpoints

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/api/libros_antiguos')
+@app.route('/api/libros_antiguo')
 def get_libros_antiguos():
     try:
         # Conecta a la base de datos SQLite
@@ -20,7 +20,45 @@ def get_libros_antiguos():
         return jsonify(libros_json)
 
     except Exception as e:
+        return jsonify({'error': str(e)}), 500        
+
+@app.route('/api/libros_nuevo')
+def get_libros_antiguos():
+    try:
+        # Conecta a la base de datos SQLite
+        conn = sqlite3.connect('biblia.db')
+        cursor = conn.cursor()
+
+        # Realiza la consulta
+        cursor.execute("SELECT id_libro, abreviacion, qnt_capitulos, nombre FROM libros WHERE testamento = 'Nuevo'")
+        libros = cursor.fetchall()
+
+        # Crea una lista de diccionarios para el resultado JSON
+        libros_json = [{'id_libro': row[0], 'abreviacion': row[1], 'qnt_capitulos': row[2], 'nombre': row[3]} for row in libros]
+
+        return jsonify(libros_json)
+
+    except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+# Endpoint para obtener los versículos de un libro específico
+@app.route('/api/versiculos_por_libro/<int:id_libro>')
+def get_versiculos_por_libro(id_libro):
+    try:
+        conn = sqlite3.connect('bilbia.db')
+        cursor = conn.cursor()
+        cursor.execute("""SELECT c.numero_capitulo, v.num_versiculo, v.texto
+                          FROM libros l
+                          INNER JOIN capitulos c ON c.id_libro = l.id_libro
+                          INNER JOIN versiculos v ON v.id_capitulo = c.id_capitulo
+                          WHERE l.id_libro = ?""", (id_libro,))
+        versiculos = cursor.fetchall()
+        versiculos_json = [{'numero_capitulo': row[0], 'num_versiculo': row[1], 'texto': row[2]} for row in versiculos]
+        return jsonify(versiculos_json)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
